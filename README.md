b6d517172a5a01fc2ed455d49cc2f7870bfe55de
Huang Yan <hyansuper@foxmail.com>
2020-01-06 02:07:50 +0800

    remotepicamera

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # PiCamera Video Streaming
 This is derived from [flask-video-streaming](https://github.com/miguelgrinberg/flask-video-streaming) by @miguelgrinberg.
 
-I made some changes to support dynamic PiCamera configuration and provide API for stream receiving:
+I made some changes to support dynamic PiCamera configuration and simple API for stream receiving:
 * run app.py on raspberry pi
-* visit http://pi-address:5000 to view video stream, you can also specify width/height/framerate/horizontal_flip/vertical_flip in the url, eg. http://pi-address:5000/?w=640&h=480&fps=10&hf=1&vf=1
-* to receive video stream from another computer, run receive.py or:
+* visit http://pi-address:5000 to view video stream, you can also specify resolution/framerate/hflip/vflip in the url, eg. http://pi-address:5000/?w=640&h=480&fps=10&hf=1&vf=1
+* to receive video stream from another computer, run receive.py or use `RemotePiCamera(pi-address, resolution=(320,240), framerate=10, hflip=False, vflip=False)`:
 ```python
 from remote_pi_camera import RemotePiCamera
 import cv2
-for frame in RemotePiCamera(pi-address, resolution=(320,240), framerate=10):
-    cv2.imshow('picam', cv2.flip(frame,-1))
+for frame in RemotePiCamera(pi-address):
+    cv2.imshow('picam', frame)
     if cv2.waitKey(1) ==27: # if user hit esc
         break
 cv2.destroyAllWindows()
--- remote_pi_camera.py
+++ remote_pi_camera.py
@@ -3,7 +3,15 @@ import cv2
 import numpy as np
 
 class RemotePiCamera:
-    def __init__(self, pi_address, resolution=(320,240), framerate=10):
+    def __init__(self, pi_address, resolution=(320,240), framerate=10, hflip=False, vflip=False):
+        if hflip and vflip:
+            self.flip = -1
+        elif hflip:
+            self.flip = 0
+        elif vflip:
+            self.flip = 1
+        else:
+            self.flip = None
         self.stream = urllib.request.urlopen('http://%s:5000/video_feed?w=%d&h=%d&fps=%d' % ((pi_address,)+resolution+(framerate,)))
         self.total_bytes = b''
     def read(self):
@@ -12,9 +20,11 @@ class RemotePiCamera:
             end = self.total_bytes.find(b'\xff\xd9') # JPEG end
             if not end == -1:
                 start = self.total_bytes.find(b'\xff\xd8') # JPEG start
-                jpg = self.total_bytes[start: end+2]
+                jpg = cv2.imdecode(np.fromstring(self.total_bytes[start: end+2], dtype=np.uint8), cv2.IMREAD_COLOR)
+                if self.flip is not None:
+                    jpg = cv2.flip(jpg, self.flip)
                 self.total_bytes = self.total_bytes[end+2:]
-                return cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR) 
+                return jpg
     def release(self):
         self.stream.close()
     def frames(self):
